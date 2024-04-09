45263118d82836c4101eb1af4118632f55dd3c8f
VlatkovicB <vlatkovicbojan@gmail.com>
2024-04-09 14:41:14 +0200

    solve group-anagrams

---
+++ group-anagrams.js
@@ -0,0 +1,29 @@
+const assert = require("./assert")
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const map = {}
+
+  for (let i = 0; i < strs.length; i++) {
+    const current = strs[i]
+    const sorted = current.split("").sort().join("")
+    if (map[sorted]) {
+      map[sorted].push(current)
+    } else {
+      map[sorted] = [current]
+    }
+  }
+
+  return Object.values(map)
+}
+
+assert(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]), [
+  ["bat"],
+  ["nat", "tan"],
+  ["ate", "eat", "tea"],
+])
+assert(groupAnagrams([""]), [[""]])
+assert(groupAnagrams(["a"]), [["a"]])
